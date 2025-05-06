4c1ff3840f856dd0f2d8ee7556ec8ffde4166f18
Gabriel Lobo <ga.p.lobo@gmail.com>
2025-05-05 22:19:15 -0300

    push all

---
+++ src/test/java/br/com/api/store/ApiStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
